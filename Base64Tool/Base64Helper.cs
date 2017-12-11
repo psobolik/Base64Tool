0a41214cbeb62239840d3baa44f6e3698463bf64
Paul Sobolik <psobolik@zoho.com>
2017-12-11 16:08:45 -0500

    First commit

---
+++ Base64Tool/Base64Helper.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Base64Tool
+{
+    public static class Base64Helper
+    {
+        private const int BucketSize = 0x1000;
+
+        #region Encode
+        // Encode from sourceFile to targetFile. If either are null then use stdin or stdout as appropriate
+        public static void Encode(string sourceFile, string targetFile, int breakCol = 0)
+        {
+            using (var inStream = OpenInStream(sourceFile))
+            {
+                using (var outStream = OpenOutStream(targetFile))
+                {
+                    Encode(inStream, outStream, breakCol);
+                }
+            }
+        }
+
+        private static int Emit(Stream outStream, byte[] bytes, int col, int breakCol)
+        {
+            foreach (var byt in bytes)
+            {
+                if (breakCol != 0 && col++ == breakCol)
+                {
+                    byte[] newLine = System.Text.Encoding.ASCII.GetBytes(Environment.NewLine);
+                    outStream.Write(newLine, 0, newLine.Length);
+                    col = 1;
+                }
+                outStream.WriteByte(byt);
+            }
+            return col;
+        }
+
+        // Encode inStream to outStream
+        // ReSharper disable once MemberCanBePrivate.Global
+        public static void Encode(Stream inStream, Stream outStream, int breakCol = 0)
+        {
+            using (var base64Transform = new ToBase64Transform())
+            {
+                var col = 0;
+                var inputBytes = new byte[base64Transform.InputBlockSize * BucketSize];
+                var outputBytes = new byte[base64Transform.OutputBlockSize];
+
+                var bytesRead = inStream.Read(inputBytes, 0, inputBytes.Length);
+                while (bytesRead != 0)
+                {
+                    var offset = 0;
+                    while (bytesRead - offset > base64Transform.InputBlockSize)
+                    {
+                        base64Transform.TransformBlock(
+                            inputBytes,
+                            offset,
+                            base64Transform.InputBlockSize,
+                            outputBytes,
+                            0);
+                        col = Emit(outStream, outputBytes, col, breakCol);
+                        offset += base64Transform.InputBlockSize;
+                    }
+                    if (bytesRead - offset > 0)
+                    {
+                        outputBytes = base64Transform.TransformFinalBlock(
+                            inputBytes,
+                            offset,
+                            bytesRead - offset);
+                        Emit(outStream, outputBytes, col, breakCol);
+                    }
+                    bytesRead = inStream.Read(inputBytes, 0, inputBytes.Length);
+                }
+            }
+
+        }
+        #endregion Encode
+
+        #region Decode
+        // Decode from sourceFile to targetFile. If either are null then use stdin or stdout as appropriate
+        public static void Decode(string sourceFile, string targetFile)
+        {
+            using (var inStream = OpenInStream(sourceFile))
+            {
+                using (var outStream = OpenOutStream(targetFile))
+                {
+                    Decode(inStream, outStream);
+                }
+            }
+        }
+
+        // Decode inStream to outStream
+        private static void Decode(Stream inStream, Stream outStream)
+        {
+            using (var base64Transform = new FromBase64Transform())
+            {
+                const int inputBlockSize = 4; // base64Transform.InputBlockSize is inexplicably == 1
+
+                var inputBytes = new byte[inputBlockSize * BucketSize];
+                var outputBytes = new byte[base64Transform.OutputBlockSize];
+
+                var bytesRead = inStream.Read(inputBytes, 0, inputBytes.Length);
+                while (bytesRead != 0)
+                {
+                    var offset = 0;
+                    while (bytesRead - offset > inputBlockSize)
+                    {
+                        var bytesTransformed = base64Transform.TransformBlock(
+                            inputBytes,
+                            offset,
+                            inputBlockSize,
+                            outputBytes,
+                            0);
+                        outStream.Write(outputBytes, 0, bytesTransformed);
+                        offset += inputBlockSize;
+                    }
+                    if (bytesRead - offset > 0)
+                    {
+                        outputBytes = base64Transform.TransformFinalBlock(
+                            inputBytes,
+                            offset,
+                            bytesRead - offset);
+                        outStream.Write(outputBytes, 0, outputBytes.Length);
+                    }
+                    bytesRead = inStream.Read(inputBytes, 0, inputBytes.Length);
+                }
+            }
+        }
+        #endregion Decode
+
+        #region Private
+        private static Stream OpenOutStream(string targetFile)
+        {
+            Stream result;
+            if (string.IsNullOrWhiteSpace(targetFile))
+            {
+                result = Console.OpenStandardOutput();
+            }
+            else
+            {
+                // The target file doesn't have to exist
+                result = new FileStream(targetFile, FileMode.Create, FileAccess.Write);
+            }
+
+            return result;
+        }
+
+        private static Stream OpenInStream(string sourceFile)
+        {
+            Stream result;
+            if (string.IsNullOrWhiteSpace(sourceFile))
+            {
+                result = Console.OpenStandardInput();
+            }
+            else
+            {
+                if (!File.Exists(sourceFile)) throw new FileNotFoundException($"Could not open file '{sourceFile}'", sourceFile);
+                result = new FileStream(sourceFile, FileMode.Open, FileAccess.Read);
+            }
+
+            return result;
+        }
+        #endregion Private
+    }
+}
---
+++ Base64Tool/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using CommandLine;
+using CommandLine.Text;
+
+namespace Base64Tool
+{
+    static class Program
+    {
+        [SuppressMessage("ReSharper", "UnusedAutoPropertyAccessor.Local")]
+        private class Options
+        {
+            [Option('b', "break", HelpText = "Break encoded string at num columns")]
+            public int BreakColumn { get; set; }
+
+            [Option('D', "decode", HelpText = "Decode input")]
+            public bool Decode { get; set; }
+
+            [Option('i', "input", HelpText = @"Input file (default: ""-"" for stdin")]
+            public string Input { get; set; }
+
+            [Option('o', "output", HelpText = @"Output file (default: ""-"" for stdout")]
+            public string Output { get; set; }
+
+            [HelpOption]
+            // ReSharper disable once UnusedMember.Local
+            public string GetUsage()
+            {
+                return HelpText.AutoBuild(this, (current) => HelpText.DefaultParsingErrorsHandler(this, current));
+            }        
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var options = new Options();
+            if (Parser.Default.ParseArgumentsStrict(args, options)) 
+            {
+                try
+                {
+                    if (options.Decode)
+                    {
+                        Base64Helper.Decode(options.Input, options.Output);
+                    }
+                    else
+                    {
+                        Base64Helper.Encode(options.Input, options.Output, options.BreakColumn);
+                    }
+                }
+                catch(Exception ex)
+                {
+                    Console.Error.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
