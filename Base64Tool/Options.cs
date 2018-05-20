e7fc9d12b3c550d39d817006a329b1626d7c741f
Paul Sobolik <psobolik@live.com>
2018-05-20 10:47:38 -0400

    Misc enhancements

---
+++ Base64Tool/Options.cs
@@ -0,0 +1,27 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+namespace Base64Tool
+{
+    internal class Options
+    {
+        [Option('b', "break", HelpText = "Break encoded string at num columns")]
+        public int BreakColumn { get; set; }
+
+        [Option('D', "decode", HelpText = "Decode input")]
+        public bool Decode { get; set; }
+
+        [Option('i', "input", HelpText = @"Input file (default: ""-"" for stdin")]
+        public string Input { get; set; }
+
+        [Option('o', "output", HelpText = @"Output file (default: ""-"" for stdout")]
+        public string Output { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this, (current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+
+}
